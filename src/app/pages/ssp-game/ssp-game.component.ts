e6ab5dbf7e92f397ad0b83ee992d77a6c362bf13
Alexander Katerinich <panik.adm@gmail.com>
2021-10-23 00:43:50 +0300

    Adding game functions

--- src/app/pages/ssp-game/ssp-game.component.ts
+++ src/app/pages/ssp-game/ssp-game.component.ts
@@ -6,11 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ssp-game.component.scss']
 })
 export class SspGameComponent implements OnInit {
+  gameElems = ['Камень', 'Ножницы', 'Бумага'];
+  isGameStarted: boolean = false;
+  AIChoice: number = 0;
+  gameResult: string = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     console.log('SspGameComponent');
+  }
+
+  getTitle(id: number): string {
+    return this.gameElems[id];
+  }
+
+  userButtonClickHandler(val: number): void {
+    this.isGameStarted = true;
+    this.AIChoice = this.AISelectEvent();
+    this.showResult(val);
+  }
+
+  showResult(val: number) {
+    if(val === this.AIChoice) {
+      this.gameResult = 'Ничья'
+    } else if (val < this.AIChoice) {
+      this.gameResult = 'Вы победили!!!'
+    } else {
+      this.gameResult = 'Вы проиграли =('
+    }
+  }
+
+  AISelectEvent(): number {
+    return Math.floor(Math.random() * 3);
   }
 
 }
